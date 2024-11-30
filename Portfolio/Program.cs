f3fa5f609157415017c9a91e14058290352e7422
Aravindkumar <myself.aravindkumar@gmail.com>
2024-12-01 03:34:56 +0530

    Removed MudBlazor

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Portfolio;
-using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +8,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBootstrapBlazor();
-builder.Services.AddMudServices();
 await builder.Build().RunAsync();
